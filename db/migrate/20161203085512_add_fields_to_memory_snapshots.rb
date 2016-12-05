92c001048d78734fb1017c1d5df85d8e38116c01
Sasha Bichkov <aleksandar.bichkov@gmail.com>
2016-12-05 19:25:55 +0300

    Remove extra spaces

--- db/migrate/20161203085512_add_fields_to_memory_snapshots.rb
+++ db/migrate/20161203085512_add_fields_to_memory_snapshots.rb
@@ -1,8 +1,8 @@
 class AddFieldsToMemorySnapshots < ActiveRecord::Migration
   def change
-    add_column :memory_snapshots, :total_allocated_objects, :bigint,  default: 0, null: false
-    add_column :memory_snapshots, :total_allocated_memsize, :bigint,  default: 0, null: false
-    add_column :memory_snapshots, :total_retained_objects,  :bigint,  default: 0, null: false
-    add_column :memory_snapshots, :total_retained_memsize,  :bigint,  default: 0, null: false
+    add_column :memory_snapshots, :total_allocated_objects, :bigint, default: 0, null: false
+    add_column :memory_snapshots, :total_allocated_memsize, :bigint, default: 0, null: false
+    add_column :memory_snapshots, :total_retained_objects,  :bigint, default: 0, null: false
+    add_column :memory_snapshots, :total_retained_memsize,  :bigint, default: 0, null: false
   end
 end
