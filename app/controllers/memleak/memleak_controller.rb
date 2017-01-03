95498290f666128f6ce531ff3762077e46ea4286
Sasha Bichkov <aleksandar.bichkov@gmail.com>
2017-01-03 15:15:25 +0300

    Get memory_snapshots between ids (from, to)

--- app/controllers/memleak/memleak_controller.rb
+++ app/controllers/memleak/memleak_controller.rb
@@ -2,11 +2,27 @@ module Memleak
   class MemleakController < ApplicationController
 
     def index
-      @memory_snapshots = MemorySnapshot.all
+      @memory_snapshots = get_memory_snapshots
     end
 
     def snapshots
-      render json: MemorySnapshot.all
+      render json: get_memory_snapshots
+    end
+
+    private
+
+    def get_memory_snapshots
+      from, to = snapshots_params[:snapshots_from], snapshots_params[:snapshots_to]
+
+      if from and to
+        MemorySnapshot.between from, to
+      else
+        MemorySnapshot.all
+      end
+    end
+
+    def snapshots_params
+      params.permit :snapshots_from, :snapshots_to
     end
 
   end
--- app/models/memory_snapshot.rb
+++ app/models/memory_snapshot.rb
@@ -1,2 +1,5 @@
 class MemorySnapshot < ActiveRecord::Base
+
+  scope :between, -> from, to { where('id >= ? AND id <= ?', from, to) }
+
 end
