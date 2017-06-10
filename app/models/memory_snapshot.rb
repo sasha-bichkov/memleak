304e9f08cd29fb04e773d49798cd6f077843e786
Sasha Bichkov <aleksandar.bichkov@gmail.com>
2017-06-10 11:51:53 +0300

    Snapshot pretty details info

--- app/models/memory_snapshot.rb
+++ app/models/memory_snapshot.rb
@@ -2,4 +2,21 @@ class MemorySnapshot < ActiveRecord::Base
 
   scope :between, -> from, to { where('id >= ? AND id <= ?', from, to) }
 
+  serialize :allocated_memory_by_gem, Array
+  serialize :allocated_objects_by_gem, Array
+  serialize :retained_memory_by_gem, Array
+  serialize :retained_objects_by_gem, Array
+  serialize :allocated_memory_by_file, Array
+  serialize :allocated_objects_by_file, Array
+  serialize :retained_memory_by_file, Array
+  serialize :retained_objects_by_file, Array
+  serialize :allocated_memory_by_location, Array
+  serialize :allocated_objects_by_location, Array
+  serialize :retained_memory_by_location, Array
+  serialize :retained_objects_by_location, Array
+  serialize :allocated_memory_by_class, Array
+  serialize :allocated_objects_by_class, Array
+  serialize :retained_memory_by_class, Array
+  serialize :retained_objects_by_class, Array
+
 end
